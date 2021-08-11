2d2a366881ff7a597173474d981bc8f0965106ff
Valeriy Savchenko <sinmipt@gmail.com>
2021-08-11 16:32:02 +0300

    Parse command-line options

--- lib/Driver/taucc.cpp
+++ lib/Driver/taucc.cpp
@@ -1,5 +1,11 @@
-#include <clang/AST/AST.h>
+#include <clang/Tooling/CommonOptionsParser.h>
 
-int main() {
+using namespace clang;
+using namespace llvm;
+
+cl::OptionCategory TauCategory("tau compiler options");
+
+int main(int Argc, const char **Argv) {
+  tooling::CommonOptionsParser OptionsParser(Argc, Argv, TauCategory);
   return 0;
 }
