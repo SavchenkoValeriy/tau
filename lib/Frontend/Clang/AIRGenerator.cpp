7bb8b50ffeb4f4cba18e7be4c57ac07e54af8652
Valeriy Savchenko <sinmipt@gmail.com>
2021-08-18 11:15:40 +0300

    Reorganize the code a bit

--- lib/Frontend/Clang/AIRGenerator.cpp
+++ lib/Frontend/Clang/AIRGenerator.cpp
@@ -57,131 +57,14 @@ public:
     G.generate();
   }
 
-  mlir::Value VisitCompoundStmt(const CompoundStmt *Compound) {
-    for (const auto *Child : Compound->body())
-      Visit(Child);
-    return nullptr;
-  }
+  mlir::Value VisitCompoundStmt(const CompoundStmt *Compound);
+  mlir::Value VisitReturnStmt(const ReturnStmt *Return);
+  mlir::Value VisitDeclStmt(const DeclStmt *DS);
 
-  mlir::Value VisitReturnStmt(const ReturnStmt *Return) {
-    mlir::Value Operand = Visit(Return->getRetValue());
-    ReturnOp Result = Builder.create<mlir::ReturnOp>(
-        Parent.loc(Return->getSourceRange()),
-        Operand ? llvm::makeArrayRef(Operand) : ArrayRef<mlir::Value>());
-    return {};
-  }
-
-  mlir::Value VisitIntegerLiteral(const IntegerLiteral *Literal) {
-    return Builder.create<ConstantIntOp>(
-        Parent.loc(Literal->getSourceRange()),
-        *Literal->getValue().getRawData(),
-        Context.getIntWidth(Literal->getType()));
-  }
-
-  mlir::Value VisitImplicitCastExpr(const ImplicitCastExpr *Cast) {
-    switch (Cast->getCastKind()) {
-    case CastKind::CK_LValueToRValue:
-      return Visit(Cast->getSubExpr());
-    default:
-      return {};
-    }
-  }
-
-  mlir::Value VisitDeclRefExpr(const DeclRefExpr *Ref) {
-    return getBinding(Ref->getDecl());
-  }
-
-  mlir::Value VisitDeclStmt(const DeclStmt *DS) {
-    for (const auto *D : DS->decls())
-      if (const auto *Var = dyn_cast<VarDecl>(D)) {
-        mlir::Value Init = Visit(Var->getInit());
-        // TODO: handle situations with undefined initializations.
-        bind(Var, Init);
-      }
-    return {};
-  }
-
-  mlir::Value VisitBinaryOperator(const BinaryOperator *BinExpr) {
-    mlir::Value LHS = Visit(BinExpr->getLHS());
-    mlir::Value RHS = Visit(BinExpr->getRHS());
-
-    mlir::Location Loc = Parent.loc(BinExpr->getSourceRange());
-
-    bool IsInteger = BinExpr->getType()->isIntegralOrEnumerationType();
-
-    mlir::Value Result;
-
-    if (IsInteger)
-      switch (BinExpr->getOpcode()) {
-      case BinaryOperatorKind::BO_PtrMemD:
-      case BinaryOperatorKind::BO_PtrMemI:
-        // TODO: support pointer-to-member operators
-        break;
-      case BinaryOperatorKind::BO_MulAssign:
-      case BinaryOperatorKind::BO_Mul:
-        Result = Builder.create<mlir::MulIOp>(Loc, LHS, RHS);
-        break;
-      case BinaryOperatorKind::BO_DivAssign:
-      case BinaryOperatorKind::BO_Div:
-      case BinaryOperatorKind::BO_RemAssign:
-      case BinaryOperatorKind::BO_Rem:
-        // TODO: support signed and unsigned division
-        break;
-      case BinaryOperatorKind::BO_AddAssign:
-      case BinaryOperatorKind::BO_Add:
-        Result = Builder.create<mlir::AddIOp>(Loc, LHS, RHS);
-        break;
-      case BinaryOperatorKind::BO_SubAssign:
-      case BinaryOperatorKind::BO_Sub:
-        Result = Builder.create<mlir::SubIOp>(Loc, LHS, RHS);
-        break;
-      case BinaryOperatorKind::BO_ShlAssign:
-      case BinaryOperatorKind::BO_Shl:
-        Result = Builder.create<mlir::ShiftLeftOp>(Loc, LHS, RHS);
-        break;
-      case BinaryOperatorKind::BO_ShrAssign:
-      case BinaryOperatorKind::BO_Shr:
-        Result = Builder.create<mlir::SignedShiftRightOp>(Loc, LHS, RHS);
-        break;
-      case BinaryOperatorKind::BO_Cmp:
-        // TODO: support spaceship operator
-        break;
-      case BinaryOperatorKind::BO_LT:
-      case BinaryOperatorKind::BO_GT:
-      case BinaryOperatorKind::BO_LE:
-      case BinaryOperatorKind::BO_GE:
-      case BinaryOperatorKind::BO_EQ:
-      case BinaryOperatorKind::BO_NE:
-        // TODO: support comparison operators
-        break;
-      case BinaryOperatorKind::BO_AndAssign:
-      case BinaryOperatorKind::BO_And:
-        Result = Builder.create<mlir::AndOp>(Loc, LHS, RHS);
-        break;
-      case BinaryOperatorKind::BO_XorAssign:
-      case BinaryOperatorKind::BO_Xor:
-        Result = Builder.create<mlir::XOrOp>(Loc, LHS, RHS);
-        break;
-      case BinaryOperatorKind::BO_OrAssign:
-      case BinaryOperatorKind::BO_Or:
-        Result = Builder.create<mlir::OrOp>(Loc, LHS, RHS);
-        break;
-      case BinaryOperatorKind::BO_LAnd:
-      case BinaryOperatorKind::BO_LOr:
-        // TODO: support logical operators
-        break;
-      case BinaryOperatorKind::BO_Assign:
-      case BinaryOperatorKind::BO_Comma:
-        Result = RHS;
-      }
-    // TODO: support FP operations
-
-    if (BinExpr->isAssignmentOp()) {
-      // TODO: get L-value of LHS and store it there.
-    }
-
-    return Result;
-  }
+  mlir::Value VisitIntegerLiteral(const IntegerLiteral *Literal);
+  mlir::Value VisitImplicitCastExpr(const ImplicitCastExpr *Cast);
+  mlir::Value VisitDeclRefExpr(const DeclRefExpr *Ref);
+  mlir::Value VisitBinaryOperator(const BinaryOperator *BinExpr);
 
 private:
   FunctionGenerator(FuncOp &ToGenerate, const FunctionDecl &Original,
@@ -345,4 +228,148 @@ OwningModuleRef AIRGenerator::generate(MLIRContext &MContext,
   MContext.loadDialect<mlir::StandardOpsDialect>();
   TopLevelGenerator ActualGenerator(MContext, Context);
   return ActualGenerator.generateModule();
+}
+
+//===----------------------------------------------------------------------===//
+//                                  Statements
+//===----------------------------------------------------------------------===//
+
+mlir::Value FunctionGenerator::VisitCompoundStmt(const CompoundStmt *Compound) {
+  for (const auto *Child : Compound->body())
+    Visit(Child);
+  return nullptr;
+}
+
+mlir::Value FunctionGenerator::VisitReturnStmt(const ReturnStmt *Return) {
+  mlir::Value Operand = Visit(Return->getRetValue());
+  ReturnOp Result = Builder.create<mlir::ReturnOp>(
+      Parent.loc(Return->getSourceRange()),
+      Operand ? llvm::makeArrayRef(Operand) : ArrayRef<mlir::Value>());
+  return {};
+}
+
+mlir::Value FunctionGenerator::VisitDeclStmt(const DeclStmt *DS) {
+  for (const auto *D : DS->decls())
+    if (const auto *Var = dyn_cast<VarDecl>(D)) {
+      mlir::Value Init = Visit(Var->getInit());
+      // TODO: handle situations with undefined initializations.
+      bind(Var, Init);
+    }
+  return {};
+}
+
+//===----------------------------------------------------------------------===//
+//                                 Expressions
+//===----------------------------------------------------------------------===//
+
+mlir::Value
+FunctionGenerator::VisitIntegerLiteral(const IntegerLiteral *Literal) {
+  return Builder.create<ConstantIntOp>(Parent.loc(Literal->getSourceRange()),
+                                       *Literal->getValue().getRawData(),
+                                       Context.getIntWidth(Literal->getType()));
+}
+
+mlir::Value
+FunctionGenerator::VisitImplicitCastExpr(const ImplicitCastExpr *Cast) {
+  switch (Cast->getCastKind()) {
+  case CastKind::CK_LValueToRValue:
+    return Visit(Cast->getSubExpr());
+  default:
+    return {};
+  }
+}
+
+mlir::Value FunctionGenerator::VisitDeclRefExpr(const DeclRefExpr *Ref) {
+  return getBinding(Ref->getDecl());
+}
+
+mlir::Value
+FunctionGenerator::VisitBinaryOperator(const BinaryOperator *BinExpr) {
+  mlir::Value LHS = Visit(BinExpr->getLHS());
+  mlir::Value RHS = Visit(BinExpr->getRHS());
+
+  mlir::Location Loc = Parent.loc(BinExpr->getSourceRange());
+
+  bool IsInteger = BinExpr->getType()->isIntegralOrEnumerationType();
+
+  mlir::Value Result;
+
+  switch (BinExpr->getOpcode()) {
+  case BinaryOperatorKind::BO_PtrMemD:
+  case BinaryOperatorKind::BO_PtrMemI:
+    // TODO: support pointer-to-member operators
+    break;
+  case BinaryOperatorKind::BO_MulAssign:
+  case BinaryOperatorKind::BO_Mul:
+    if (IsInteger)
+      Result = Builder.create<mlir::MulIOp>(Loc, LHS, RHS);
+    else
+      Result = Builder.create<mlir::MulFOp>(Loc, LHS, RHS);
+    break;
+  case BinaryOperatorKind::BO_DivAssign:
+  case BinaryOperatorKind::BO_Div:
+  case BinaryOperatorKind::BO_RemAssign:
+  case BinaryOperatorKind::BO_Rem:
+    // TODO: support signed and unsigned division
+    break;
+  case BinaryOperatorKind::BO_AddAssign:
+  case BinaryOperatorKind::BO_Add:
+    if (IsInteger)
+      Result = Builder.create<mlir::AddIOp>(Loc, LHS, RHS);
+    else
+      Result = Builder.create<mlir::AddFOp>(Loc, LHS, RHS);
+    break;
+  case BinaryOperatorKind::BO_SubAssign:
+  case BinaryOperatorKind::BO_Sub:
+    if (IsInteger)
+      Result = Builder.create<mlir::SubIOp>(Loc, LHS, RHS);
+    else
+      Result = Builder.create<mlir::SubFOp>(Loc, LHS, RHS);
+    break;
+  case BinaryOperatorKind::BO_ShlAssign:
+  case BinaryOperatorKind::BO_Shl:
+    Result = Builder.create<mlir::ShiftLeftOp>(Loc, LHS, RHS);
+    break;
+  case BinaryOperatorKind::BO_ShrAssign:
+  case BinaryOperatorKind::BO_Shr:
+    Result = Builder.create<mlir::SignedShiftRightOp>(Loc, LHS, RHS);
+    break;
+  case BinaryOperatorKind::BO_Cmp:
+    // TODO: support spaceship operator
+    break;
+  case BinaryOperatorKind::BO_LT:
+  case BinaryOperatorKind::BO_GT:
+  case BinaryOperatorKind::BO_LE:
+  case BinaryOperatorKind::BO_GE:
+  case BinaryOperatorKind::BO_EQ:
+  case BinaryOperatorKind::BO_NE:
+    // TODO: support comparison operators
+    break;
+  case BinaryOperatorKind::BO_AndAssign:
+  case BinaryOperatorKind::BO_And:
+    Result = Builder.create<mlir::AndOp>(Loc, LHS, RHS);
+    break;
+  case BinaryOperatorKind::BO_XorAssign:
+  case BinaryOperatorKind::BO_Xor:
+    Result = Builder.create<mlir::XOrOp>(Loc, LHS, RHS);
+    break;
+  case BinaryOperatorKind::BO_OrAssign:
+  case BinaryOperatorKind::BO_Or:
+    Result = Builder.create<mlir::OrOp>(Loc, LHS, RHS);
+    break;
+  case BinaryOperatorKind::BO_LAnd:
+  case BinaryOperatorKind::BO_LOr:
+    // TODO: support logical operators
+    break;
+  case BinaryOperatorKind::BO_Assign:
+  case BinaryOperatorKind::BO_Comma:
+    Result = RHS;
+  }
+  // TODO: support FP operations
+
+  if (BinExpr->isAssignmentOp()) {
+    // TODO: get L-value of LHS and store it there.
+  }
+
+  return Result;
 }
