29190c991a88bb39e0142b6c765125920fd84fe6
Valeriy Savchenko <sinmipt@gmail.com>
2021-09-21 18:50:02 +0300

    Fix getStateAttributes function for empty cases

--- lib/Core/State.cpp
+++ lib/Core/State.cpp
@@ -7,5 +7,5 @@ using namespace mlir;
 
 ArrayRef<Attribute> tau::core::getStateAttributes(Operation *Op) {
   auto Attr = Op->getAttrOfType<ArrayAttr>(StateAttrID);
-  return Attr.getValue();
+  return Attr ? Attr.getValue() : ArrayRef<Attribute>();
 }
