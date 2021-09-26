0bb87ec01d4835ab2da51280477b0b6286330b56
Valeriy Savchenko <sinmipt@gmail.com>
2021-09-26 14:27:43 +0300

    Expand the basic test for flow-sensitive analysis

--- unittests/Core/FlowSensitiveTest.cpp
+++ unittests/Core/FlowSensitiveTest.cpp
@@ -2,19 +2,25 @@
 #include "tau/Core/Checker.h"
 #include "tau/Core/CheckerPass.h"
 #include "tau/Core/State.h"
+#include "tau/Core/StateEventForest.h"
 #include "tau/Frontend/Clang/Clang.h"
 #include "tau/Frontend/Output.h"
 
 #include <clang/Tooling/Tooling.h>
 #include <initializer_list>
+#include <iterator>
 #include <llvm/ADT/ArrayRef.h>
+#include <llvm/ADT/STLExtras.h>
+#include <llvm/ADT/SmallVector.h>
 #include <llvm/ADT/StringSwitch.h>
+#include <llvm/Support/Casting.h>
 #include <memory>
 #include <mlir/Dialect/StandardOps/IR/Ops.h>
 
 #include <gtest/gtest.h>
 #include <mlir/Pass/Pass.h>
 #include <mlir/Pass/PassManager.h>
+#include <mlir/Support/LLVM.h>
 
 using namespace tau;
 using namespace core;
@@ -60,8 +66,9 @@ class FlowSensitiveIssuesHarvester
     : public PassWrapper<FlowSensitiveIssuesHarvester, OperationPass<FuncOp>> {
 public:
   FlowSensitiveIssuesHarvester(
-      ArrayRef<FlowSensitiveAnalysis::Issue> &IssuesToFill)
-      : FoundIssues(IssuesToFill) {}
+      std::vector<FlowSensitiveAnalysis::Issue> &IssuesToFill,
+      StateEventForest &Forest)
+      : FoundIssues(IssuesToFill), Forest(Forest) {}
 
   StringRef getArgument() const override { return "flow-sen-checker"; }
   StringRef getDescription() const override {
@@ -70,41 +77,51 @@ public:
 
   void runOnOperation() override {
     auto &FlowSen = getAnalysis<FlowSensitiveAnalysis>();
-    FoundIssues = FlowSen.getFoundIssues();
-  }
 
-  ArrayRef<FlowSensitiveAnalysis::Issue> getFoundIssues() const {
-    return FoundIssues;
+    // We need to make sure that we own the issues.
+    llvm::copy(FlowSen.getFoundIssues(), std::back_inserter(FoundIssues));
+
+    // We also need to keep forest alive, so that events are also alive
+    // when the analysis is gone.
+    Forest = std::move(FlowSen.getEventForest());
+    markAllAnalysesPreserved();
   }
 
 private:
-  ArrayRef<FlowSensitiveAnalysis::Issue> &FoundIssues;
+  std::vector<FlowSensitiveAnalysis::Issue> &FoundIssues;
+  StateEventForest &Forest;
 };
 
 class FlowSensitiveAnalysisTest : public ::testing::Test {
 public:
-  template <class CheckerT>
-  ArrayRef<FlowSensitiveAnalysis::Issue> run(const Twine &Snippet) {
-    auto IR = frontend::runClangOnCode(Snippet);
-    MLIRContext &Context = IR->Context;
-    Context.printOpOnDiagnostic(false);
+  template <class CheckerT> void run(const Twine &Snippet) {
+    IR = frontend::runClangOnCode(Snippet);
+    ASSERT_NE(IR, nullptr);
 
+    MLIRContext &Context = IR->Context;
     PassManager PM(&Context);
+
     CheckerT TestChecker;
     SmallVector<Checker *, 1> Checkers{&TestChecker};
-    PM.addNestedPass<FuncOp>(createCheckerPass(Checkers));
-    ArrayRef<FlowSensitiveAnalysis::Issue> FoundIssues;
-    PM.addNestedPass<FuncOp>(
-        std::make_unique<FlowSensitiveIssuesHarvester>(FoundIssues));
-    EXPECT_TRUE(succeeded(PM.run(IR->Module)));
 
-    return FoundIssues;
+    PM.addNestedPass<FuncOp>(createCheckerPass(Checkers));
+    PM.addNestedPass<FuncOp>(std::make_unique<FlowSensitiveIssuesHarvester>(
+        FoundIssues, EventForest));
+
+    ASSERT_TRUE(succeeded(PM.run(IR->Module)));
   }
+
+private:
+  std::unique_ptr<frontend::Output> IR;
+  StateEventForest EventForest;
+
+protected:
+  std::vector<FlowSensitiveAnalysis::Issue> FoundIssues;
 };
 } // end anonymous namespace
 
 TEST_F(FlowSensitiveAnalysisTest, Basic) {
-  auto Issues = run<SimpleChecker>(R"(
+  run<SimpleChecker>(R"(
 void foobar(int &x) {}
 void foo(int &x) {}
 void bar(int &x) {}
@@ -115,5 +132,25 @@ void test(int x) {
   foobar(x);
 }
 )");
-  ASSERT_EQ(Issues.size(), 1);
+  ASSERT_EQ(FoundIssues.size(), 1);
+  auto Issue = FoundIssues[0];
+  EXPECT_TRUE(Issue.Guaranteed);
+
+  auto Foobar = dyn_cast_or_null<CallOp>(Issue.ErrorEvent.Location);
+  ASSERT_TRUE(Foobar);
+  EXPECT_TRUE(Foobar.callee().startswith("void foobar"));
+
+  ASSERT_NE(Issue.ErrorEvent.Parent, nullptr);
+  auto BarEvent = *Issue.ErrorEvent.Parent;
+  auto Bar = dyn_cast_or_null<CallOp>(BarEvent.Location);
+  ASSERT_TRUE(Bar);
+  EXPECT_TRUE(Bar.callee().startswith("void bar"));
+
+  ASSERT_NE(BarEvent.Parent, nullptr);
+  auto FooEvent = *BarEvent.Parent;
+  auto Foo = dyn_cast_or_null<CallOp>(FooEvent.Location);
+  ASSERT_TRUE(Foo);
+  EXPECT_TRUE(Foo.callee().startswith("void foo"));
+
+  ASSERT_EQ(FooEvent.Parent, nullptr);
 }
