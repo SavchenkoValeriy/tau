ccc7c99b23a0bf2a57fa2915e3285b59fde91af0
Valeriy Savchenko <sinmipt@gmail.com>
2024-09-08 11:39:11 +0100

    Extend use-after-free checker to handle new/delete

--- test/Checkers/UseAfterFree/trivial.cpp
+++ test/Checkers/UseAfterFree/trivial.cpp
@@ -7,7 +7,7 @@ struct A {
 int test() {
   int *x;
   {
-    int a = 42;
+    int a = 42; // expected-note{{Allocated here}}
     x = &a;
   } // expected-note{{Deallocated here}}
   return *x; // expected-error{{Use of deallocated pointer}}
@@ -17,8 +17,21 @@ int test_field() {
   A b;
   b.x = 0;
   {
-    int a = 42;
+    int a = 42; // expected-note{{Allocated here}}
     b.x = &a;
   } // expected-note{{Deallocated here}}
+  return *b.x; // expected-error{{Use of deallocated pointer}}
+}
+
+int test_new_delete() {
+  int *x = new int(42); // expected-note{{Allocated here}}
+  delete x; // expected-note{{Deallocated here}}
+  return *x; // expected-error{{Use of deallocated pointer}}
+}
+
+int test_new_delete_field() {
+  A b;
+  b.x = new int(42); // expected-note{{Allocated here}}
+  delete b.x; // expected-note{{Deallocated here}}
   return *b.x; // expected-error{{Use of deallocated pointer}}
 }
