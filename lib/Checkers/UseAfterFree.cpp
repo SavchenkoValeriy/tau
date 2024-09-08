ca601057fe4c226e8a675894e94093405eb0f3d2
Valeriy Savchenko <sinmipt@gmail.com>
2024-09-08 11:39:11 +0100

    Extend use-after-free checker to handle new/delete

--- lib/Checkers/UseAfterFree.cpp
+++ lib/Checkers/UseAfterFree.cpp
@@ -9,14 +9,16 @@ using namespace core;
 using namespace air;
 using namespace mlir;
 
-using FreeState = core::State<1, 1>;
-constexpr FreeState FREED = FreeState::getNonErrorState(0);
+using FreeState = core::State<2, 1>;
+constexpr FreeState ALLOCATED = FreeState::getNonErrorState(0);
+constexpr FreeState FREED = FreeState::getNonErrorState(1);
 constexpr FreeState ERROR = FreeState::getErrorState(0);
 
 consteval auto makeStateMachine() {
   StateMachine<FreeState> SM;
+  SM.markInitial(ALLOCATED);
+  SM.addEdge(ALLOCATED, FREED);
   SM.addEdge(FREED, ERROR);
-  SM.markInitial(FREED);
   return SM;
 }
 
@@ -24,13 +26,22 @@ constexpr auto SM = makeStateMachine();
 
 namespace {
 
-class UseAfterFree : public CheckerBase<UseAfterFree, FreeState, SM, LoadOp,
-                                        GetFieldPtr, DeallocaOp> {
+class UseAfterFree
+    : public CheckerBase<UseAfterFree, FreeState, SM, AllocaOp, HeapAllocaOp,
+                         LoadOp, GetFieldPtr, DeallocaOp, HeapDeallocaOp> {
 public:
   StringRef getName() const override { return "Use after free checker"; }
   StringRef getArgument() const override { return "use-after-free"; }
   StringRef getDescription() const override {
     return "Detect uses of deallocated values";
+  }
+
+  void process(AllocaOp Alloca) const {
+    markResultChange<ALLOCATED>(Alloca.getOperation());
+  }
+
+  void process(HeapAllocaOp HeapAlloca) const {
+    markResultChange<ALLOCATED>(HeapAlloca.getOperation());
   }
 
   void process(LoadOp Load) const {
@@ -42,7 +53,12 @@ public:
   }
 
   void process(DeallocaOp Dealloca) const {
-    markChange<FREED>(Dealloca.getOperation(), Dealloca.getPtr());
+    markChange<ALLOCATED, FREED>(Dealloca.getOperation(), Dealloca.getPtr());
+  }
+
+  void process(HeapDeallocaOp HeapDealloca) const {
+    markChange<ALLOCATED, FREED>(HeapDealloca.getOperation(),
+                                 HeapDealloca.getPtr());
   }
 
   InFlightDiagnostic emitError(mlir::Operation *Op, FreeState State) {
@@ -52,8 +68,11 @@ public:
 
   void emitNote(InFlightDiagnostic &Diag, mlir::Operation *Op,
                 FreeState State) {
-    assert(State == FREED);
-    Diag.attachNote(Op->getLoc()) << "Deallocated here";
+    if (State == FREED) {
+      Diag.attachNote(Op->getLoc()) << "Deallocated here";
+    } else {
+      Diag.attachNote(Op->getLoc()) << "Allocated here";
+    }
   }
 
   MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(UseAfterFree);
